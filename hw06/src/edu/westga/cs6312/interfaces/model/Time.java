5df347c12a3689cbd9f5c488bd6a3ad5b122997e
jim.perry.jr - m.a.c.s. <jperry11@my.westga.edu>
2016-02-18 00:02:39 -0500

    + 0 parameter constructor that sets object to midnight

--- hw06/src/edu/westga/cs6312/interfaces/model/Time.java
+++ hw06/src/edu/westga/cs6312/interfaces/model/Time.java
@@ -21,9 +21,9 @@ public class Time {
 	 * @param minute 	unit of time in minutes
 	 * @param second 	unit of time in seconds
 	 * 
-	 * Precondition: hour < 0 || hour > 24, minute < 0 || minute > 60, second < 0 || second > 60
-	 * Postcondition: An animal of whose type is kind and is
-	 *                covered with covering
+	 * Precondition: 	hour < 0 || hour > 24, minute < 0 || minute > 60, second < 0 || second > 60
+	 * Postcondition: 	hour > -1 && hour < 25, minute > -1 && minute < 61, second > -1 && second < 61
+	 * 			 		set minute, hour and second variables
 	 */ 	
 	private Time (int hour, int minute, int second) {
 		if (hour < 0 || hour > 24) {
@@ -46,4 +46,44 @@ public class Time {
 		}
 		
 	}
+	
+	
+	/** 
+	 * 2 parameter constructor that accepts the 
+	 * hour and minute values for the new time object
+	 * @param hour 		unit of time hours
+	 * @param minute 	unit of time in minutes
+	 * 
+	 * Precondition: 	hour < 0 || hour > 24, minute < 0 || minute > 60, second < 0 || second > 60
+	 * Postcondition: 	hour > -1 && hour < 25, minute > -1 && minute < 61, set minute, hour variables
+	 */ 	
+	private Time (int hour, int minute) {
+		if (hour < 0 || hour > 24) {
+			throw new IllegalArgumentException("Invalid number, hour must no be less than 0");
+		}
+		if (minute < 0 || minute > 60) {
+			throw new IllegalArgumentException("Invalid number, minute must no be less than 0");
+		}		
+		if (hour > -1 && hour < 25){
+			this.hour = hour;
+		}
+		if (minute > -1 && minute < 61){
+			this.minute = minute;
+		}
+		this.second = 0;		
+	}	
+	
+	/** 
+	 * 0 parameter constructor that 
+	 * initializes the object to midnight
+	 */ 	
+	private Time () {
+		this.hour = 0;		
+		this.hour = 0;
+		this.minute = 0;
+	}
+			
+	
+	
+	
 }
